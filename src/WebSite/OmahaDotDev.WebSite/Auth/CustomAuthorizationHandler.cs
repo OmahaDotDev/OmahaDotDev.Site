143f7e9cfd4681bda94e7d7eba97ce5bc0696182
Brian Olson <beolson@hero4hire.com>
2022-11-17 10:58:58 -0600

    add missing files

---
+++ src/WebSite/OmahaDotDev.WebSite/Auth/CustomAuthorizationHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace OmahaDotDev.WebSite.Auth;
+
+public class CustomAuthorizationRequirement: IAuthorizationRequirement
+{
+    
+}
+public class CustomAuthorizationHandler: AuthorizationHandler<CustomAuthorizationRequirement>
+{
+    protected override Task 
+        HandleRequirementAsync(AuthorizationHandlerContext context, CustomAuthorizationRequirement requirement)
+    {
+
+        if (
+            context.User.Identity is { IsAuthenticated: true, Name: "beolson@gmail.com" } &&
+            context.User.FindFirstValue(ClaimTypes.NameIdentifier)?.Length > 10
+        )
+        {
+            context.Succeed(requirement);
+        }
+
+        return Task.CompletedTask;
+    }
+}
